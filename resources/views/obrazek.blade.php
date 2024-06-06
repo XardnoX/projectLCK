bbe18baa290e65dd8a49d35cad7c2db94295aad7
cahel_filip <cahel_filip@oauh.cz>
2024-06-06 13:01:43 +0200

    done all

--- resources/views/obrazek.blade.php
+++ resources/views/obrazek.blade.php
@@ -7,7 +7,7 @@
 <body class="d-flex flex-column min-vh-100">
     
 <!-- Create a new file in resources/views/obrazek.blade.php -->
-<img src="{{   $obrazek->nazev) }}" alt="obrazek">
+<img src="{{ ($obrazek->nazev) }}" alt="obrazek">
     
 </body>
 </html>
